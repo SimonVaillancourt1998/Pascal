7f583bbebc71b0d1ace546f0c7d37f56fd04c809
Simon Vaillancourt <vaillancourt.smn98@gmail.com>
2024-04-26 22:42:39 -0400

    Checkpoint

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from openai import OpenAI
+from dotenv import dotenv
+
+env = dotenv()
+
+client = OpenAI(api_key=env.PASCAL_KEY)
+
+assistant = client.beta.assistants.retrieve(assistant_id="asst_pjOwrgeGnK2EkakZ1Tx3GULc")
 
 
-
-
-
-PASCAL = OpenAI(api_key=)
+if __name__ == "__main__":
+    pass    
